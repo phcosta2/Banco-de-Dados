490d138b1a1d5c7286fbdc1d1877f3079973b532
jvgoverna <jv11governa2018@gmail.com>
2024-05-11 21:48:56 -0300

    Adicionando valores (testes)nas tabelas

--- projeto.py
+++ projeto.py
@@ -27,11 +27,8 @@ CREATE TABLE IF NOT EXISTS Departamento(
 criar_tabela_professor = """
 CREATE TABLE IF NOT EXISTS Professor(
     id_professor varchar(12) PRIMARY KEY,
-
     nome_professor varchar(20),
-
     salario FLOAT DEFAULT 0,
-
     nome_departamento varchar(30) REFERENCES Departamento(nome_departamento)
 )
 """
@@ -110,6 +107,49 @@ cursor.execute(criar_tabela_aluno)
 cursor.execute(criar_tabela_horas_complementares)
 cursor.execute(criar_tabela_historico_escolar)
 
+cursor.execute("DELETE FROM departamento")
+cursor.execute("INSERT INTO DEPARTAMENTO VALUES ('Matematica','Leonardo Anjoletto')")
+cursor.execute("INSERT INTO DEPARTAMENTO VALUES ('Ciencias da Computacao','Matheus Gomes')")
+
+cursor.execute("DELETE FROM professor")
+cursor.execute("INSERT INTO PROFESSOR VALUES ('123456789','Gustavo',5000)")
+cursor.execute("INSERT INTO PROFESSOR VALUES ('987654321','Bruno',2000)")
+
+cursor.execute("DELETE FROM curso")
+cursor.execute("INSERT INTO CURSO VALUES ('CC', 'Ciencias da Computacao',260)")
+cursor.execute("INSERT INTO CURSO VALUES ('EP', 'Engenharia de Producao',220)")
+
+cursor.execute("DELETE FROM materia")
+cursor.execute("INSERT INTO MATERIA VALUES ('CC2648','Banco de Dados','P2')")
+cursor.execute("INSERT INTO MATERIA VALUES ('CC1948','Calculo 1','Projeto')")
+
+cursor.execute("DELETE FROM matriz_curricular")
+cursor.execute("INSERT INTO MATRIZ_CURRICULAR VALUES ('Segundo',2024,12)")
+cursor.execute("INSERT INTO MATRIZ_CURRICULAR VALUES ('Primeiro',2023,1)")
+
+cursor.execute("DELETE FROM tcc")
+cursor.execute("INSERT INTO TCC VALUES ('1224058962','Conhecimentos de Banco de Dados')")
+cursor.execute("INSERT INTO TCC VALUES ('122402','Conhecimentos de Redes')")
+
+cursor.execute("DELETE FROM aluno")
+cursor.execute("INSERT INTO ALUNO VALUES ('24.122.027-6','João',19)")
+cursor.execute("INSERT INTO ALUNO VALUES ('24.122.055-7','Felipe',21)")
+
+cursor.execute("DELETE FROM horas_complementares")
+cursor.execute("INSERT INTO HORAS_COMPLEMENTARES VALUES ('A05','curso de banco de dados',50)")
+cursor.execute("INSERT INTO HORAS_COMPLEMENTARES VALUES ('A03','curso de Redes',30)")
+
+cursor.execute("DELETE FROM historico_escolar")
+cursor.execute("INSERT INTO HISTORICO_ESCOLAR VALUES ('12212121',10,'quinto')")
+cursor.execute("INSERT INTO HISTORICO_ESCOLAR VALUES ('1222',7,'segundo')")
+
+
 
 
 conexao.commit()
+
+
+# Arrumar a quantidade de caracteres dentro do varchar
+# Remover o ID horas de chave primária em horas complementares (ele pode ser repetido)
+# Perguntar se pode utilizar o python para inserir dados
+# Perguntar sobre os dados null
