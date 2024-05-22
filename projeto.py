1872eb2621c25b2922befbca5689e29063e8a5f8
Pedro Henrique Lega Kramer Costa <143679336+phcosta2@users.noreply.github.com>
2024-05-22 19:50:17 -0300

    Deixando código bonito
    @FelipeOrlandoLanzara 
    @jvgoverna

--- projeto.py
+++ projeto.py
@@ -7,6 +7,7 @@ from faker import Faker
 from faker.providers import DynamicProvider
 import random
 
+# Ler os dados do arquivo.json (database,user,password,host,port) e armazená-los na variável config
 with open('acess.json') as file:
     config = json.load(file)
 
@@ -46,16 +47,16 @@ nome_materia = DynamicProvider(
 # Geracao de semestres do historico do professor de maneira aleatoria
 semestre = DynamicProvider(
     provider_name = "semestres",
-    elements = ['Primeiro', 'Segundo' ,'Terceiro' ,'Quarto' ,'Quinto' ,'Sexto' ,'Sétimo' ,'Oitavo' ,'Nono' ,'Décimo']
+    elements = ['Primeiro', 'Segundo' ,'Terceiro' ,'Quarto' ,'Quinto' ,'Sexto' ,'Sétimo' ,'Oitavo']
 )
 
 
 lista_materias = [num for num in random.sample(range(100000, 999999), 60)]
 
 # Gera 100 números únicos entre 100000000 e 500000000 servir para os RAs dos alunos
-ra_aluno = random.sample(range(100000000, 500000000), 10)
+ra_aluno = random.sample(range(100000000, 500000000), 50)
 # Gera 60 números únicos entre 500000001 e 999999999 servir para os RAs dos profesores
-ra_professor = random.sample(range(500000001, 999999999), 10)
+ra_professor = random.sample(range(500000001, 999999999), 20)
 # Formata os números no padrão xx.xxx.xxx-x e adicionar na lista com list compehension
 ids_aluno = [f"{str(num)[:2]}.{str(num)[2:5]}.{str(num)[5:8]}-{str(num)[8]}" for num in ra_aluno]
 # Formata os números no padrão xx.xxx.xxx-x e adicionar na lista com list compehension
@@ -66,7 +67,6 @@ lista_hist_escolar = [num for num in random.sample(range(1, 500000), 60)]
 lista_hist_professor = [num for num in random.sample(range(500001, 999999), 60)]
 
 print('inicialização das listas')
-
 
 #inicialização dos providers:
 fake.add_provider(nome_materia)
@@ -108,8 +108,7 @@ for linha in range(len(primary_keys["nome_departamento"])):
     cursor.execute("INSERT INTO DEPARTAMENTO VALUES (%s, %s)", (
     primary_keys["nome_departamento"][linha],
     fake.first_name()
-    ))#TABELA COMPLETA
-
+    ))
 
 # Criação das tabelas do Professor
 for linha in range(len(ids_prof)):
@@ -118,7 +117,7 @@ for linha in range(len(ids_prof)):
     fake.first_name(), 
     random.randint(2000,20000),
     random.choice(primary_keys["nome_departamento"])
-    ))#TABELA COMPLETA
+    ))
 
 #Criação das tabelas da Materia:
 for linha in range(len(lista_materias)):
@@ -127,23 +126,20 @@ for linha in range(len(lista_materias)):
     fake.materias(), 
     fake.pybool(), 
     ids_prof[random.randint(0, (len(ids_prof)-1))]
-    ))#TABELA INCOMPLETA
-
+    ))
 # PRECISA ESCREVER O NOME DE DEPARTAMENTO CORRETO NA MATÉRIA BASEADO NO QUE ESTA NA TABELA DO PROFESSOR
 cursor.execute("SELECT id_professor, nome_departamento FROM PROFESSOR")
 professores = cursor.fetchall() 
 for _ in professores:
     cursor.execute("UPDATE MATERIA SET nome_departamento = %s WHERE id_professor = %s", (_[1], _[0]))
 
-
 #Criação da Tabela Curso:
 for linha in range(len(primary_keys["id_curso"])):
     cursor.execute("INSERT INTO CURSO (id_curso, horas_extras) VALUES (%s, %s)", (
     primary_keys["id_curso"][linha] ,
     random.randint(160,300)
-    ))#TABELA INCOMPLETA
-#FALTOU NOME DO CURSO E NOME DO DEPARTAMENTO
-#1) 
+    ))
+
 #Atualizar a tabela curso com departamento, nome e id
 cursor.execute("UPDATE CURSO SET nome_departamento = 'Matemática' WHERE id_curso = 'MA'")
 cursor.execute("UPDATE CURSO SET nome_departamento = 'Física' WHERE id_curso = 'FI'")
@@ -168,17 +164,13 @@ cursor.execute("UPDATE CURSO SET nome_curso = 'Engenharia de Produção' WHERE N
 cursor.execute("UPDATE CURSO SET nome_curso = 'Engenharia Nuclear' WHERE NOME_DEPARTAMENTO = 'Engenharia Nuclear'")
 cursor.execute("UPDATE CURSO SET nome_curso = 'Engenharia Textil' WHERE NOME_DEPARTAMENTO = 'Engenharia Textil'")
 
-
-
 #Criação da tabela Matriz Curricular
 for materia in lista_materias:
     cursor.execute("INSERT INTO MATRIZ_CURRICULAR (id_materia) VALUES (%s)", (
         str(materia),
         ))#TABELA INCOMPLETA
 
-# PRECISA ESCREVER O NOME DO DEPARTAMENTO BASEADO NO ID_MATERIA PRESENTE EM MATERIA
-
-# Atualizar a tabela de matérias com o departamento baseado no id do prof
+#Atualizar a tabela de matérias com o departamento baseado no id do prof
 #Escrever materia e id_curso em matriz curricular puxando da materia
 cursor.execute("SELECT id_materia, nome_departamento FROM MATERIA")
 materiaa = cursor.fetchall() #criar tupla com todos os dados da tabela
@@ -196,12 +188,12 @@ cursor.execute("UPDATE MATRIZ_CURRICULAR SET id_curso = 'EP' WHERE materia = 'En
 cursor.execute("UPDATE MATRIZ_CURRICULAR SET id_curso = 'EN' WHERE materia = 'Engenharia Nuclear' ")
 cursor.execute("UPDATE MATRIZ_CURRICULAR SET id_curso = 'ET' WHERE materia = 'Engenharia Textil'")
 
-#Criação das tabelas do TCC: - precisa colocar titulos decentes
+#Criação das tabelas do TCC:
 for linha in range(len(ids_aluno)):
     cursor.execute("INSERT INTO TCC (titulo,id_professor) VALUES (%s,%s)", (
         "Título " + str(linha),
           ids_prof[random.randint(0, (len(ids_prof)-1))]
-          ))#TABELA COMPLETA
+          ))
 
 #Criação das tabelas do Aluno:
 for linha in range(len(ids_aluno)):
@@ -211,11 +203,7 @@ for linha in range(len(ids_aluno)):
         random.randint(18,65),
         primary_keys["id_curso"][random.randint(0, 4)], 
         (linha + 1)
-        ))#TABELA COMPLETA
-  
-
-#<VERIFICADO ATÉ ESSE PONTO por Pedro>
-
+        ))
 
 #Criação da tabela de Horas Complementares
 for linha in range(len(ids_aluno)):
@@ -250,111 +238,15 @@ for linha in range(len(lista_hist_professor)):
         lista_materias[random.randint(0, (len(lista_materias)-1))]
         ))
 
-
-
-
-
-
-# criacao das queries
-# 1- histórico escolar de qualquer aluno, retornando o código e nome da disciplina, semestre e ano que a disciplina foi cursada e nota final
-# query1 = """
-#     CREATE VIEW query1 AS
-#         SELECT m.id_materia, m.nome_materia, he.semestre, he.ano, he.nota 
-#         FROM historico_escolar he
-#         INNER JOIN materia m ON he.id_materia = m.id_materia
-# """
-# cursor.execute(query1)
-
-# query1_1 = """
-#     CREATE VIEW query1_1 AS
-#         WITH qualquer_pessoa AS (
-#             SELECT nome_aluno
-#             FROM aluno
-#             ORDER BY RANDOM()
-#             LIMIT 1
-#         )
-#         SELECT a.nome_aluno, m.id_materia, m.nome_materia, he.semestre, he.ano, he.nota 
-#         FROM historico_escolar he
-#         INNER JOIN materia m ON he.id_materia = m.id_materia
-#         INNER JOIN aluno a ON a.id_aluno = he.id_aluno
-#         WHERE a.nome_aluno = (
-#             SELECT nome_aluno
-#             FROM qualquer_pessoa
-#         )
-# """
-# cursor.execute(query1_1)
-
-# # 2- histórico de disciplinas ministradas por qualquer professor, com semestre e ano
-# query2 = """
-#     CREATE VIEW query2 AS 
-#         WITH qualquer_professor AS (
-#             SELECT nome_professor
-#             FROM professor
-#             ORDER BY RANDOM()
-#             LIMIT 1
-#         )
-#         SELECT p.nome_professor, hp.id_materia, hp.semestre, hp.ano
-#         FROM professor p 
-#         INNER JOIN historico_professor hp ON p.id_professor = hp.id_professor
-#         WHERE p.nome_professor = (
-#             SELECT nome_professor
-#             FROM qualquer_professor
-#         )
-# """
-# cursor.execute(query2)
-
-# # 3- listar alunos que já se formaram (foram aprovados em todos os cursos de uma matriz curricular) em um determinado semestre de um ano
-# query3 = """
-#     CREATE VIEW query3 AS
-#         WITH qualquer AS (
-#             SELECT semestre, ano
-#             FROM historico_escolar
-#             ORDER BY RANDOM()
-#             LIMIT 1
-#         )
-#         SELECT a.nome_aluno, c.nome_curso, c.id_curso, he.nota, he.semestre, he.ano
-#         FROM aluno a
-#         INNER JOIN curso c ON a.id_curso = c.id_curso
-#         INNER JOIN historico_escolar he ON a.id_aluno = he.id_aluno 
-#         WHERE he.semestre = (
-#             SELECT semestre
-#             FROM qualquer
-#         ) AND he.ano = (
-#             SELECT ano
-#             FROM qualquer
-#         ) AND he.nota >= 5
-# """
-# cursor.execute(query3)
-
-# # 4- listar todos os professores que são chefes de departamento, junto com o nome do departamento
+# 4- listar todos os professores que são chefes de departamento, junto com o nome do departamento
 cursor.execute("INSERT INTO DEPARTAMENTO VALUES (%s, %s)", ('Astrofisica', 'Julia'))
 cursor.execute("INSERT INTO PROFESSOR VALUES (%s, %s, %s, %s)", ('12.456.789-0', 'Julia', 25000, 'Astrofisica'))
-# query4 = """
-#     CREATE VIEW query4 AS
-#         SELECT p.nome_professor, d.nome_departamento
-#         FROM professor p
-#         INNER JOIN departamento d ON p.nome_departamento = d.nome_departamento
-#         WHERE p.nome_professor = d.chefe_departamento
-# """
-# cursor.execute(query4)
-
-# # 5- saber quais alunos formaram um grupo de TCC e qual professor foi o orientador
-# query5 = """
-#     CREATE VIEW query5 AS
-#         SELECT a.nome_aluno, p.nome_professor
-#         FROM aluno a
-#         INNER JOIN tcc t ON t.id_tcc = a.id_tcc 
-#         INNER JOIN professor p ON p.id_professor = t.id_professor
-# """
-# cursor.execute(query5)
 
 with open("query.sql", "r") as sql_file:
     sql_script = sql_file.read()
 
 # executar o SQL script
 cursor.execute(sql_script)
-
-
 
 conexao.commit()
 print("Sucesso")
